0ee6220e3d1dd9d77eebee567f32e892c0c38299
Patricio Rojas <patricio.m.rojas.r@gmail.com>
2023-07-21 13:02:34 -0400

    :package: Plain Text sin input

--- src/components/MarmotaRow/MarmotaRow.vue
+++ src/components/MarmotaRow/MarmotaRow.vue
@@ -100,6 +100,30 @@
           ></v-simple-checkbox>
         </div>
 
+        <!-- PLAIN TEXT -->
+        <div
+          v-if="headerItem.type == 'plain-text'"
+          class="d-flex flex-row align-center flex-grow-1"
+        >
+          <div class="prefix">
+            {{
+              get(item, headerItem, 'prefix') != null
+                ? get(item, headerItem, 'prefix')
+                : ''
+            }}
+          </div>
+          <div class="plain-text">
+            {{ get(item, headerItem, 'value') }}
+          </div>
+          <div class="suffix">
+            {{
+              get(item, headerItem, 'suffix') != null
+                ? get(item, headerItem, 'suffix')
+                : ''
+            }}
+          </div>
+        </div>
+
         <!-- TEXT -->
         <div
           v-if="headerItem.type == 'text'"
